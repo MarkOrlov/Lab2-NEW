7cd2f6dc04bd3bb9ac99ce31075abc071ecbb002
Mark <machello1995@gmail.com>
2015-10-15 20:38:13 +0400

    3.1

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -1,11 +1,11 @@
 #include "sdt.h"
-
+//Учебная программа
 int main()
 {
     cout <<"Input A & B \n";
     int a,b;
     cin >>a >>b;
-    cout <<"Sum A & B : \n"<< (a+b);
+    cout <<"Sum A & B :"<< (a+b);
     cout <<"Razn A & B :"<< (a-b);
-
+    cout <<"Proizv A & B :"<< (a*b);
 }
