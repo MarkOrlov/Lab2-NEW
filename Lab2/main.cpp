8318f89ced0f67ac42ebc5befb7179cbbc80034e
Mark <machello1995@gmail.com>
2015-10-15 21:05:35 +0400

    final

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -5,11 +5,11 @@ int main()
     cout <<"Input A & B \n";
     int a,b;
     cin >>a >>b;
-    cout <<"Sum A & B :"<< (a+b);
-    cout <<"Razn A & B :"<< (a-b);
-<<<<<<< .merge_file_a01408
-    cout <<"proizved A & B :"<< (a*b);
-=======
-    cout <<"Delen A & B :"<< (a/b);
->>>>>>> .merge_file_a08056
+    cout <<"Sum A & B :"<< (a+b) <<"\n";
+    cout <<"Razn A & B :"<< (a-b) <<"\n";
+
+    cout <<"proizved A & B :"<< (a*b) <<"\n";
+
+    cout <<"Delen A & B :"<< (a/b) <<"\n";
+
 }
